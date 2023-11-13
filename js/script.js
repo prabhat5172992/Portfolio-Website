49f84cd1652c5e2f09dfda795bb94a631208dfac
prabhat5172992 <prabhat5172992@gmail.com>
2023-11-14 02:31:50 +0530

    more changes added

--- js/script.js
+++ js/script.js
@@ -22,7 +22,12 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+ id +']').classList.add('active');
-            })
+            });
+
+            // active section for animation on scroll
+            sec.classList.add('show-animate');
+        } else {  // animation that repeats on scroll
+            sec.classList.remove('show-animate');
         }
     })
 
